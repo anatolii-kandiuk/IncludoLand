781b304cdc3ee16be4a1b43e09cda07f64498657
anatolii <batkovich327@gmail.com>
2025-09-25 15:09:05 +0200

    [IL-3] - Updated App Structure. Created modules: IL-Liquibase and IL-App

--- includoland-app/src/main/java/com/includoland/includolandapp/IncludolandAppApplication.java
+++ includoland-app/src/main/java/com/includoland/includolandapp/IncludolandAppApplication.java
@@ -1,8 +1,11 @@
 package com.includoland.includolandapp;
 
+import com.includoland.includolandliquibase.config.IncludolandLiquibaseConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(IncludolandLiquibaseConfig.class)
 @SpringBootApplication
 public class IncludolandAppApplication {
 
