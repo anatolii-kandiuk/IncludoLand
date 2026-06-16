115cad8098b1cd0cee6b81a7289154cfe2a96cff
anatolii <batkovich327@gmail.com>
2026-06-16 15:09:19 +0200

    first commit

---
+++ includoland/src/main/java/com/includoland/includoland/IncludolandApplication.java
@@ -0,0 +1,13 @@
+package com.includoland.includoland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncludolandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncludolandApplication.class, args);
+	}
+
+}
---
+++ includoland/src/test/java/com/includoland/includoland/IncludolandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.includoland.includoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncludolandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
