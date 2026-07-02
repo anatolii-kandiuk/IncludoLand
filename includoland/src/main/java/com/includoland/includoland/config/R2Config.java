7398f82a8954e6a053c80d8cc4c6d88777562234
anatolii <batkovich327@gmail.com>
2026-07-02 10:04:56 +0200

    Created R2 Config

---
+++ includoland/src/main/java/com/includoland/includoland/config/R2Config.java
@@ -0,0 +1,39 @@
+package com.includoland.includoland.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class R2Config {
+
+    @Value("${cloud.r2.endpoint}")
+    private String endpoint;
+
+    @Value("${cloud.r2.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.r2.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .region(Region.of("auto"))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true) // Обов'язково для R2
+                        .build())
+                .build();
+    }
+}
